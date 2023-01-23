0ccde0fd6038907dd3cfdf16c5f46d73f539ef22
Denys Bondar <bondar.denis.php@gmail.com>
2023-01-23 12:12:15 +0200

    simple chat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,14 +8,47 @@
      </head>
     <body>
 
+<ul id="list"></ul>
+
+    <hr>
+    <form action="">
+        <textarea style="widows: 100%; height: 50px;"></textarea>
+        <input type="submit" value="send">
+    </form>
+
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.5.4/socket.io.js"></script>
 
     <script>
         var socket = io(':6001');
         socket.on('message', function(data){
             console.log('from server:', data)
+        });
 
-        })
+        socket.on('server-info', function(data){
+            console.log('from server:', data)
+        });
+
+        function appendMessage(data)
+        {
+            var ul = document.getElementById("list");
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(data.message));
+            ul.appendChild(li);
+
+        }
+
+        $('form').on('submit', function () {
+            var text = $('textarea').val(),
+                msg = {message: text};
+
+            socket.send(msg);
+            appendMessage(msg);
+
+            $('textarea').val('');
+            return false;
+        });
+
     </script>
 
     </body>
--- ws.server/server.js
+++ ws.server/server.js
@@ -10,5 +10,28 @@ redis = new Redis();
 io.on('connection', function(socket) {
     console.log('New connection', socket.id);
 
-    socket.send('msg from server');
+    // //send msg
+    // socket.send('msg from server');
+    //
+    // //Fire event
+    // socket.emit('server-info', {version: .1 });
+    //
+    //
+    // //join to room
+    // socket.join('vip', function (error){
+    //     console.log(socket.rooms)
+    // });
+
+    // socket.on('message', function(data) {
+    //     socket.broadcast.send(data);
+    //
+    // });
+
+
+    socket.on('message', function (data) {
+        console.log(data);
+        io.sockets.send(data);
+
+    });
+
 })
